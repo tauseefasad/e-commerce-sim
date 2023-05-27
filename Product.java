fe20f0f9d6834250f7b7e986fd55c98b9ee1f9c5
Tauseef <82678141+tauseefasad@users.noreply.github.com>
2023-05-27 11:11:59 -0400

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,134 @@
+
+//MOHAMMED TAUSEEF ASAD
+//501135583
+
+
+/*
+ * class Product defines a product for sale by the system.
+ *
+ * A product belongs to one of the 5 categories below.
+ *
+ * Some products also have various options (e.g. size, color, format, style, ...). The options can affect
+ * the stock count(s). In this generic class Product, product options are not used in get/set/reduce stockCount() methods
+ *
+ * Some products
+ */
+public class Product
+{
+	public static enum Category {GENERAL, CLOTHING, BOOKS, FURNITURE, COMPUTERS};
+
+	private String name;
+	private String id;
+	private com.company.Product.Category category;
+	private double price;
+	private int stockCount;
+
+	public Product()
+	{
+		this.name = "Product";
+		this.id = "001";
+		this.category = com.company.Product.Category.GENERAL;
+		this.stockCount = 0;
+	}
+
+	public Product(String id)
+	{
+		this("Product", id, 0, 0, com.company.Product.Category.GENERAL);
+	}
+
+	public Product(String name, String id, double price, int stock, com.company.Product.Category category)
+	{
+		this.name = name;
+		this.id = id;
+		this.price = price;
+		this.stockCount = stock;
+		this.category = category;
+	}
+	/*
+	 * This method always returns true in class Product. In subclasses, this method will be overridden
+	 * and will check to see if the options specified are valid for this product.
+	 */
+	public boolean validOptions(String productOptions)
+	{
+		return true;
+	}
+
+	public com.company.Product.Category getCategory()
+	{
+		return category;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public double getPrice()
+	{
+		return price;
+	}
+
+	public void setPrice(double price)
+	{
+		this.price = price;
+	}
+
+	/*
+	 * Return the number of items currently in stock for this product
+	 * Note: in this general class, the productOptions parameter is not used. It may be used
+	 * in subclasses.
+	 */
+	public int getStockCount(String productOptions)
+	{
+		return stockCount;
+	}
+	/*
+	 * Set (or replenish) the number of items currently in stock for this product
+	 * Note: in this general class, the productOptions parameter is not used. It may be used
+	 * in subclasses.
+	 */
+	public void setStockCount(int stockCount, String productOptions)
+	{
+		this.stockCount = stockCount;
+	}
+	/*
+	 * Reduce the number of items currently in stock for this product by 1 (called when a product has
+	 * been ordered by a customer)
+	 * Note: in this general class, the productOptions parameter is not used. It may be used
+	 * in subclasses.
+	 */
+	public void reduceStockCount(String productOIptions)
+	{
+		stockCount--;
+	}
+
+	public void print()
+	{
+		System.out.printf("\nId: %-5s Category: %-9s Name: %-20s Price: %7.1f", id, category, name, price);
+	}
+
+	/*
+	 * Two products are equal if they have the same product Id.
+	 * This method is inherited from superclass Object and overridden here
+	 */
+	public boolean equals(Object other)
+	{
+		com.company.Product otherP = (com.company.Product) other;
+		return this.id.equals(otherP.id);
+	}
+}
---
+++ README.txt
@@ -0,0 +1,38 @@
+E-Commerce Simulator
+
+Name: TAUSEEF ASAD
+
+OUTPUTS:
+
+CUSTS: It runs and compiles, prints the list of customers without errors.
+
+BOOKS: It runs and compiles, prints the list of books without errors.
+
+ORDERS: It runs and compiles, prints the list of orders without errors.
+
+SHIPPED: It runs and compiles, prints the list of shipped orders without errors.
+
+ORDER: It runs and compiles without errors, checks for valid customer or product ID, displays error message if incorrect product is chosen by the user. 
+If product choice is valid, it reduces the stock accordingly. 
+
+SHIP: It runs and compiles without errors.
+
+CUSTORDERS: It runs and compiles without errors.
+
+NEWCUST: It runs and compiles without errors.
+
+CANCEL: It runs and compiles without errors.
+
+ORDERBOOK: It runs and compiles without errors. Checks if product chosen is a book and displays error if other products were selected. 
+If product choice is valid, it reduces the stock accordingly.  
+
+ORDERSHOES: It runs and compiles without errors. Checks if product chosen is a shoe and displays error if other products were selected. 
+If product choice is valid, it reduces the stock accordingly.
+
+SORTBYPRICE: It runs and compiles without errors, prints the list of products in ascending order of price.
+
+SORTBYNAME: It runs and compiles without errors, prints the list of products alphabetically.
+
+SORTCUSTS: It runs and compiles without errors, prints the list of customers alphabetically.
+
+BOOKSBYAUTHOR: It runs and compiles without errors, prints the books written by selected author in ascending order of year published.
